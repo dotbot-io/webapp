0de72a76d901026cb265645293aaf2195aa9d82e
Ludovico Russo <ludus.russo@gmail.com>
2016-01-21 12:34:17 +0100

    Add rostest

--- app/ardu/views.py
+++ app/ardu/views.py
@@ -7,28 +7,32 @@ from ..models import Sketch
 from .. import db
 
 
-@ardu.route('/') 
+@ardu.route('/')
 def index():
 	return render_template('ardu/cover.html')
 
-@ardu.route('/edit/<int:id>') 
+@ardu.route('/edit/<int:id>')
 def edit(id):
 	s = Sketch.query.get_or_404(id)
 	return render_template('ardu/arduino.html', sketch=s)
 
 
-@ardu.route('/programs') 
+@ardu.route('/programs')
 def sketches():
     return render_template('ardu/sketches.html', current_time=datetime.utcnow())
 
-@ardu.route('/monitor') 
+@ardu.route('/monitor')
 def monitor():
     return render_template('ardu/monitor.html')
 
-@ardu.route('/rosnodes') 
+@ardu.route('/rosnodes')
 def rosnodes():
     return render_template('ardu/rosnodes.html', current_time=datetime.utcnow())
 
-@ardu.route('/rostopics') 
+@ardu.route('/rostopics')
 def rostopics():
     return render_template('ardu/rostopics.html', current_time=datetime.utcnow())
+
+@ardu.route('/test')
+def test_page():
+    return render_template('ardu/test.html')
