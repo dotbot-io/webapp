6d2850087c36d703309b61515e4ffd6da794f094
Ludovico Russo <ludus.russo@gmail.com>
2016-01-24 14:57:30 +0100

    rm gunicorn support

--- manage.py
+++ manage.py
@@ -19,30 +19,8 @@ manager.add_command('db', MigrateCommand)
 def test():
 	"""Run the unit tests."""
 	import unittest
-	tests = unittest.TestLoader().discover('tests') 
+	tests = unittest.TestLoader().discover('tests')
 	unittest.TextTestRunner(verbosity=2).run(tests)
 
-class GunicornServer(Command):
-    """Run the app within Gunicorn"""
-
-    def get_options(self):
-        from gunicorn.config import make_settings
-
-        settings = make_settings()
-        options = (
-            Option(*klass.cli, action=klass.action)
-            for setting, klass in settings.iteritems() if klass.cli
-        )
-        return options
-
-    def run(self, *args, **kwargs):
-        from gunicorn.app.wsgiapp import WSGIApplication
-
-        app = WSGIApplication()
-        app.app_uri = 'manage:app'
-        return app.run()
-
-manager.add_command("gunicorn", GunicornServer())
-
-if __name__ == '__main__': 
+if __name__ == '__main__':
 	manager.run()
