f5c3a57c56a855863ceb9124020b7931df7f6a26
Ludovico Russo <ludus.russo@gmail.com>
2016-01-30 22:31:23 +0100

    test quasi funzionante

--- app/static/js/rostest.js
+++ app/static/js/rostest.js
@@ -60,5 +60,29 @@ var setMotors = function(element, num) {
   });
   console.log(speed);
   cmdMotor.publish(speed);
-
 }
+
+
+console.log("touchscreen is", VirtualJoystick.touchScreenAvailable() ? "available" : "not available");
+
+var joystick	= new VirtualJoystick({
+  container	: document.getElementById('joypad'),
+  mouseSupport	: true,
+});
+joystick.addEventListener('touchStart', function(){
+  console.log('down')
+})
+joystick.addEventListener('touchEnd', function(){
+  console.log('up')
+})
+
+setInterval(function(){
+  var outputEl	= document.getElementById('result');
+  outputEl.innerHTML	= '<b>Result:</b> '
+    + ' dx:'+joystick.deltaX()
+    + ' dy:'+joystick.deltaY()
+    + (joystick.right()	? ' right'	: '')
+    + (joystick.up()	? ' up'		: '')
+    + (joystick.left()	? ' left'	: '')
+    + (joystick.down()	? ' down' 	: '')
+}, 1/30 * 1000);
