42871c4cd20fae801e0b2a8dd867cc0bd67fae63
Ludovico Russo <ludus.russo@gmail.com>
2016-02-09 00:56:35 +0100

    post

--- manage.py
+++ manage.py
@@ -15,7 +15,7 @@ def make_shell_context():
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
-print os.getenv('FLASK_CONFIG')
+print 'post =', os.getenv('FLASK_CONFIG')
 
 @manager.command
 def test():
