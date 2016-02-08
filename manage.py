3aa6727ae0da0d019418af89c7cf5af62e56517b
Ludovico Russo <ludus.russo@gmail.com>
2016-02-09 00:38:59 +0100

    up manage

--- manage.py
+++ manage.py
@@ -17,8 +17,8 @@ manager.add_command('db', MigrateCommand)
 
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
-api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+api.upgrade(app.config["SQLALCHEMY_DATABASE_URI"], app.config["SQLALCHEMY_MIGRATE_REPO"])
+v = api.db_version(app.config["SQLALCHEMY_DATABASE_URI"], app.config["SQLALCHEMY_MIGRATE_REPO"])
 print('Current database version: ' + str(v))
 
 @manager.command
