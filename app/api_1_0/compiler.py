83c3a0a6b5be72b6a62a4479cdd5465f760f8286
Ludovico Russo <ludus.russo@gmail.com>
2016-02-09 22:26:14 +0100

    speed up compilation

--- app/api_1_0/compiler.py
+++ app/api_1_0/compiler.py
@@ -40,7 +40,7 @@ class Compiler:
         if (Compiler.wall == True):
             return False
         Compiler.wall = True
-        self.proc = subprocess.Popen(['catkin_make'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=current_app.config["CATKIN_FOLDER"], env=self.env)
+        self.proc = subprocess.Popen(['catkin_make', 'robotoma_app_node'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=current_app.config["CATKIN_FOLDER"], env=self.env)
 
         return True
 
