a7b2659ce33bc613268c0180c20f6fc2f07da4d6
Ludovico Russo <ludus.russo@gmail.com>
2016-11-03 12:34:31 +0100

    discovery on cloud

---
+++ app/api_1_0/cloud.py
@@ -0,0 +1,18 @@
+from flask import Flask, current_app
+from flask_restful import Resource, Api
+
+from flask_cors import CORS, cross_origin
+from flask import jsonify
+
+from . import api
+
+restapi = Api(api)
+cors = CORS(api, resources={r"/": {"origins": "*"}})
+
+class Robot(Resource):
+    decorators = [cross_origin(origin="*", headers=["content-type", "autorization"])]
+
+    def get(self):
+        return jsonify({'name': g.DOTBOT_NAME, 'master': g.MASTER_URL, 'ip': g.ROS_IP})
+
+api.add_resource(Robot, '/discovery')
