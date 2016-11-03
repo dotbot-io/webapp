036bd48fb87d0e4f0a9cf158ffcba7e6f9e6c568
Ludovico Russo <ludus.russo@gmail.com>
2016-11-03 15:18:30 +0100

    discovery on cloud

--- app/api_1_0/cloud.py
+++ app/api_1_0/cloud.py
@@ -5,6 +5,7 @@ from flask_cors import CORS, cross_origin
 from flask_json import as_json
 
 from . import api
+from compile import comp
 
 
 rest_api = Api(api)
@@ -14,7 +15,20 @@ class Robot(Resource):
     def get(self):
         return jsonify({'name': current_app.config["DOTBOT_NAME"], 'master': current_app.config["ROS_MASTER_URI"], 'ip': current_app.config["ROS_IP"]})
 
+class RobotSketch(Resource):
+    decorators = [cross_origin(origin="*", headers=["content-type", "autorization"])]
+    def get(self):
+        id = 1
+        parser = reqparse.RequestParser()
+        parser.add_argument('code')
+        args = parser.parse_args()
+    	n = Node.query.get_or_404(id)
+    	comp.run(n)
+    	return Response(comp.read_run_proc(id), mimetype='text/event-stream')
+
+
 
 
 
 rest_api.add_resource(Robot, '/discovery')
+rest_api.add_resource(RobotSketch, '/run/sketch')
