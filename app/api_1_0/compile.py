3d5c1649173b58a5afe29fb8346f7e1d2fb3f7cc
Ludovico Russo <ludus.russo@gmail.com>
2016-11-03 14:08:20 +0100

    discovery on cloud

--- app/api_1_0/compile.py
+++ app/api_1_0/compile.py
@@ -50,8 +50,3 @@ def api_kill_node(id):
 		comp.kill_node(n.id)
 		return json_response(running=comp.is_runnning(n.id))
 	raise JsonError(error='node not in database')
-
-@api.route('/discovery')
-def test():
-    #return {'name': current_app.config["ROS_MASTER_URI"], 'master': current_app.config["MASTER_URL"], 'ip': current_app.config["ROS_IP"]}
-    return jsonify({'name': current_app.config["DOTBOT_NAME"], 'master': current_app.config["ROS_MASTER_URI"], 'test': current_app.config["ROS_IP"]})
