a79c69a37b3c085777f39ad70df5577bbbb0643b
Ludovico Russo <ludus.russo@gmail.com>
2016-11-03 14:04:06 +0100

    discovery on cloud

--- app/api_1_0/cloud.py
+++ app/api_1_0/cloud.py
@@ -8,11 +8,16 @@ from . import api
 
 cors = CORS(api, resources={r"/": {"origins": "*"}})
 
-@api.route('/rosnode/<path:node>/', methods=['DELETE'])
-@as_json
-def rostopic_kill(node):
-    env = comp.env()
-    env["ROS_NAMESPACE"] = '';
-    print env
-    subprocess.Popen(['rosnode', 'kill', node], env=env)
-    return json_response( response='ok')
+# @api.route('/rosnode/<path:node>/', methods=['DELETE'])
+# @as_json
+# def rostopic_kill(node):
+    # env = comp.env()
+    # env["ROS_NAMESPACE"] = '';
+    # print env
+    # subprocess.Popen(['rosnode', 'kill', node], env=env)
+    # return json_response( response='ok')
+
+@api.route('/discovery')
+def test():
+    #return {'name': current_app.config["ROS_MASTER_URI"], 'master': current_app.config["MASTER_URL"], 'ip': current_app.config["ROS_IP"]}
+    return jsonify({'name': current_app.config["DOTBOT_NAME"], 'master': current_app.config["ROS_MASTER_URI"], 'test': current_app.config["ROS_IP"]})
