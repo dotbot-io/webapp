d9fa234bb0e1d55ccfd8c0966b5d2a4b17fe55c9
Ludovico Russo <ludus.russo@gmail.com>
2016-11-25 12:15:50 +0100

    add wifi

---
+++ app/api_1_0/wifi.py
@@ -0,0 +1,76 @@
+from flask import Flask, current_app, g, jsonify, Response, request
+from flask_restful import Resource, Api
+
+from flask_cors import CORS, cross_origin
+from flask_json import JsonError, json_response, as_json
+from flask_restful import Api, Resource, reqparse
+
+from . import api
+from compile import comp
+from ..models import Node, File
+from datetime import datetime
+from .. import db
+import subprocess
+
+from .wifi import rest_api
+from wifi import Cell, Scheme
+
+
+
+def WifiConnectio(Resource):
+    def get(self):
+        cells = Cell.all('wlan0')
+        return jsonify([{'name': c.ssid} for c in cells])
+
+
+def getMAC(interface):
+    try:
+        str = open('/sys/class/net/' + interface + '/address').read()
+    except:
+        str = "00:00:00:00:00:00"
+    return str[0:17]
+
+class Robot(Resource):
+    decorators = [cross_origin(origin="*", headers=["content-type", "autorization"], methods=['GET', 'PUT'])]
+    def get(self):
+        return jsonify({'name': current_app.config["DOTBOT_NAME"], 'master': current_app.config["ROS_MASTER_URI"], 'ip': current_app.config["ROS_IP"], "macaddress":getMAC('wlan0'), "model":"dotbot-ros b0.5"})
+
+class RobotSketch(Resource):
+    decorators = [cross_origin()]
+
+    def put(self):
+        node_id = 27
+        file_id = 53
+    	f = File.query.get_or_404(file_id)
+        parser = reqparse.RequestParser()
+        parser.add_argument('code')
+        args = parser.parse_args()
+
+    	f.code = args['code']
+    	f.last_edit = datetime.utcnow()
+    	db.session.add(f)
+    	f.save()
+        db.session.commit()
+
+    	n = Node.query.get_or_404(node_id)
+    	comp.run(n)
+    	return jsonify({'response': 'ok'})
+
+    def get(self):
+        print 'getting streaming'
+        node_id = 27
+        return Response(comp.read_run_proc(node_id), mimetype='text/event-stream')
+
+    def delete(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('node')
+        args = parser.parse_args()
+        env = comp.env()
+        env["ROS_NAMESPACE"] = '';
+        subprocess.Popen(['rosnode', 'kill', args.node], env=env)
+    	return jsonify({'response': 'ok'})
+
+
+
+rest_api.add_resource(Robot, '/discovery')
+rest_api.add_resource(RobotSketch, '/run/sketch')
