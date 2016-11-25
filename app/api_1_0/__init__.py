398da8e82ad13ed122981cc0938dfde5479bd963
Ludovico Russo <ludus.russo@gmail.com>
2016-11-25 12:20:16 +0100

    add wifi

--- app/api_1_0/__init__.py
+++ app/api_1_0/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 api = Blueprint('api', __name__)
 
-from . import compile, roshandler, rasp_commands, nodes, files, cloud
+from . import compile, roshandler, rasp_commands, nodes, files, cloud, wifi
