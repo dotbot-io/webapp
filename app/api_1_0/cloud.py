82be2dc81bbe58a98ed114a3e22e5bc321f5e2d1
Ludovico Russo <ludus.russo@gmail.com>
2016-11-25 12:46:18 +0100

    add wifi

--- app/api_1_0/cloud.py
+++ app/api_1_0/cloud.py
@@ -68,9 +68,8 @@ class WifiConnection(Resource):
         cells = Cell.all('wlan0')
         wifi_info = []
         for c in cells:
-            w_info = {'name': c.ssid, 'encryption': c.encryption_type, 'encrypted': c.encrypted}
-            if w_info != "\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00":
-                wifi_info.append(w_info)
+            if c.ssid is not in [wc.name for wc in wifi_info] + ["\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00"]
+                wifi_info.append({'name': c.ssid, 'encryption': c.encryption_type, 'encrypted': c.encrypted})
         return jsonify({'wifi': wifi_info})
 
 
