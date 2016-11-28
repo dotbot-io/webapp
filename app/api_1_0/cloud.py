4cac4b7bf8371c0bf862298bfcffb5fd36e026e7
Ludovico Russo <ludus.russo@gmail.com>
2016-11-28 16:04:43 +0100

    add wifi

--- app/api_1_0/cloud.py
+++ app/api_1_0/cloud.py
@@ -134,10 +134,21 @@ class WifiScheme(Resource):
             if len(cells) == 0:
                 return jsonify({'error': 'wifi not found'})
             sname = scheme.name
-            scheme.delete()
-            scheme = Scheme.for_cell('wlan0', sname, cells[0], args['password'])
-            scheme.save()
+
+            for s in Scheme.all():
+                s.delete()
+                if s.name = sname:
+                    s = Scheme.for_cell('wlan0', sname, cells[0], args['password'])
+                s.save()
             return jsonify({'scheme': scheme.__dict__})
+        elif args["action"] == "clean":
+            sname = scheme.name
+            for s in Scheme.all():
+                s.delete()
+                if s.name = sname:
+                    s = Scheme('wlan0', sname)
+                s.save()
+
         else:
             return jsonify({'scheme': scheme.__dict__})
 
