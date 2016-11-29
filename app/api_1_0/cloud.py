12531ad749a6c58fab3ab818341f178c85a027e6
Ludovico Russo <ludus.russo@gmail.com>
2016-11-29 16:55:00 +0100

    up sse server

--- app/api_1_0/cloud.py
+++ app/api_1_0/cloud.py
@@ -36,7 +36,7 @@ def option_autoreply():
         # Allow the origin which made the XHR
         h['Access-Control-Allow-Origin'] = request.headers['Origin']
         # Allow the actual method
-        h['Access-Control-Allow-Methods'] = ['PUT', 'POST', 'GET', 'OPTIONS', 'DELETE']
+        h['Access-Control-Allow-Methods'] = h['Allow']
         # Allow for 10 seconds
         h['Access-Control-Max-Age'] = "10"
 
